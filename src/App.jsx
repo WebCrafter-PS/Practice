be277f7807a554c51ae204076a3e4c5f7b8e7e2c
Anamika-UI26 <priyakshetty26@gmail.com>
2025-02-25 13:15:08 +0530

    star

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,37 @@ import Search from "./components/SearchBar";
 import Accordian from "./components/Accordian";
 import DropDown from "./components/DropDown";
 import TwoDropDown from "./components/TwoDropdown";
-import Tooltip from "./components/Tooltip"
+import Tooltip from "./components/Tooltip";
 import Theme from "./components/Theme";
 import { useState, useContext } from "react";
 import { ThemeContext } from "./utility/context";
 import OTP from "./components/OTPgenerator";
 import PasswordGenerator from "./components/PasswordGen";
+import StarRating from "./components/StarRating";
 
 function App() {
   const data = useContext(ThemeContext);
   const { theme } = data;
   const [mode, setMode] = useState(theme);
-  
+
   return (
     <>
       <div className="app">
         <Todo />
-        <Counter/>
-        <Toggle/>
-        <Timer/>
-        <Tooltip/>
-        <Accordian/>
-        <Search/> 
-        <DropDown/>
-        <TwoDropDown/>
-        <ThemeContext.Provider value={{ theme: mode,setMode }}>
+        <Counter />
+        <Toggle />
+        <Timer />
+        <Tooltip />
+        <Accordian />
+        <Search />
+        <DropDown />
+        <TwoDropDown />
+        <ThemeContext.Provider value={{ theme: mode, setMode }}>
           <Theme />
         </ThemeContext.Provider>
-        <OTP/>
-        <PasswordGenerator/>
+        <OTP />
+        <PasswordGenerator />
+        <StarRating />
       </div>
     </>
   );
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,2 +1,32 @@
-//star rating component allows users to rate and provide feedback 
+//star rating component allows users to rate and provide feedback
+
+import { useState } from "react";
+
 //on content or products by selecting a specified number of stars.
+const StarRating = () => {
+  const [starArray, setStarArray] = useState(Array(5).fill("empty"));
+
+  const clickHandler = (index) => {
+    const updatedStars = starArray.map((_, i) =>
+      i <= index ? "fill" : "empty"
+    );
+    setStarArray(updatedStars)
+  };
+  return (
+    <>
+      <h1>Rating</h1>
+      <div className="star">
+        {starArray.map((color, index) => (
+          <span
+            className={`${color}`}
+            key={index}
+            onClick={() => clickHandler(index)}
+          >
+            ★
+          </span>
+        ))}
+      </div>
+    </>
+  );
+};
+export default StarRating;
